edf18a42eaa52ce136f9cde67e068d5bda72f2b4
Seun Taiwo <lorddro1532@gmail.com>
2025-01-06 11:04:41 +0100

    fix min-w bug

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         }}
       ></div>
 
-      <nav className="text-white flex justify-between items-center px-5 py-10 relative z-10 mx-auto min-[900px]:px-[100px] lg:max-w-7xl lg:mx-auto">
+      <nav className="text-white flex justify-between items-center px-5 py-10 relative z-10 mx-auto ml:px-[100px] lg:max-w-7xl lg:mx-auto">
         <div className="flex items-center">
           <Image className="mr-2" src={handeeLogo} alt="handee logo" />
           <h1 className="font-cabin font-bold text-lg leading-[21.8px]">
@@ -150,23 +150,23 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <footer className="px-5 pb-10 w-full relative z-10 bg-transparent max-w-[520px] mx-auto min-[900px]:px-0 min-[900px]:max-w-[unset]">
+        <footer className="px-5 pb-10 w-full relative z-10 bg-transparent max-w-[520px] mx-auto ml:px-0 ml:max-w-none">
           <div
-            className="py-[1px] relative lg:max-w-7xl lg:mx-auto after:content[''] after:hidden after:absolute after:-left-[1px] after:top-0 after:h-full after:w-[2px] after:bg-[#020202] before:content[''] before:absolute before:-right-[1px] before:top-0 before:h-full before:w-[2px] before:bg-[#020202] before:hidden min-[1300]:after:block min-[1300]:before:block"
+            className="py-[1px] relative lg:max-w-7xl lg:mx-auto after:content[''] after:hidden after:absolute after:-left-[1px] after:top-0 after:h-full after:w-[2px] after:bg-[#020202] before:content[''] before:absolute before:-right-[1px] before:top-0 before:h-full before:w-[2px] before:bg-[#020202] before:hidden xlf:after:block xlf:before:block"
             style={{
               background:
                 "conic-gradient(from 180.41deg at 50% 94%, #2FD0D7 0deg, #F2F2F2 178.69deg, #418DF4 342.67deg, #2FD0D7 360deg)",
             }}
           >
-            <div className="w-full py-9 bg-[#020202] min-[900px]:flex min-[900px]:justify-center min-[900px]:items-center min-[900px]:px-[100px] min-[900px]:py-12 ">
-              <div className="min-[900px]:flex min-[900px]:items-center min-[900px]:mr-4">
+            <div className="w-full py-9 bg-[#020202] ml:flex ml:justify-center ml:items-center ml:px-[100px] ml:py-12 ">
+              <div className="ml:flex ml:items-center ml:mr-4">
                 <Image
-                  className="mx-auto mb-2 min-[900px]:mr-2"
+                  className="mx-auto mb-2 ml:mr-2"
                   src={handeeLogo}
                   alt="handee logo"
                 />
                 <p
-                  className="text-xl font-medium text-center my-2 mb-4 bg-clip-text text-transparent max-w-[340px] mx-auto min-[900px]:max-w-[unset]"
+                  className="text-xl font-medium text-center my-2 mb-4 bg-clip-text text-transparent max-w-[340px] mx-auto ml:max-w-[unset]"
                   style={{
                     backgroundImage:
                       "linear-gradient(90deg, #F68740 0%, #EE4E7C 30%, #4377B2 65%, #14B397 100%)",
@@ -175,7 +175,7 @@ export default function Home() {
                   Follow us on our socials to stay updated !
                 </p>
               </div>
-              <div className="flex justify-center items-center space-x-5 w-auto mx-auto min-[900px]:mx-0 lg:mr-0">
+              <div className="flex justify-center items-center space-x-5 w-auto mx-auto ml:mx-0 lg:mr-0">
                 {/* <div className="bg-[#212121] rounded-2xl p-4">
                   <Image src={google} alt="google social media icon" />
                 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,15 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      ml: "900px",
+      lg: "1024px",
+      xl: "1280px",
+      xlf: "1300px",
+      "2xl": "1536px",
+    },
     extend: {
       colors: {
         background: "var(--background)",
